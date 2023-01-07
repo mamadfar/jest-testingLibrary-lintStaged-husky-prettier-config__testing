d51c8e445439afa1e3177486a57f2b3f6c7f1d4b
mohammad farhadi <m.sabah.farhadi@gmail.com>
2023-01-07 18:08:26 +0100

    add coverage treshold

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,12 @@ module.exports = {
       },
     ],
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
